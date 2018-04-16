5c9936fe0c9e765e4c1b6da595e7bb5b2b1e7963
Alfret <Afisse13@gmail.com>
2018-04-16 14:17:46 +0200

    Updated LSP code parsing. Errors and warnings are now collected from all compilation units and stored in the program database with the actual programs.

--- src/lsp/LSP/Producer.hs
+++ src/lsp/LSP/Producer.hs
@@ -1,42 +1,136 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module LSP.Producer (
-    preCheckProgramTable,
-    typeCheckProgramTable
+    produceProgramFromSource
 ) where
 
-{- Run a pre-check on the program table before the type-checking pass
+-- ###################################################################### --
+-- Section: Imports
+-- ###################################################################### --
 
-Param: Program table to run pre-check on
+-- Library
+import Text.Megaparsec
+
+-- Standard
+import qualified Data.List.NonEmpty as NE(head)
+import Language.Haskell.TH
+import System.Environment
+import System.Exit
+import qualified Data.Map.Strict as Map
+import Control.Monad
+
+-- Encore
+import Parser.Parser
+import AST.AST
+import AST.Desugarer
+import AST.PrettyPrinter
+import ModuleExpander
+import Typechecker.Environment
+import Typechecker.Prechecker(precheckProgram)
+import Typechecker.Typechecker(typecheckProgram, checkForMainClass)
+import Typechecker.TypeError
+import Utils
+
+-- LSP
+import LSP.Database
+
+-- ###################################################################### --
+-- Section: Support
+-- ###################################################################### --
+
+-- the following line of code resolves the standard path at compile time using Template Haskell
+standardLibLocation = $(stringE . init =<< runIO (System.Environment.getEnv "ENCORE_MODULES" ))
+
+preludePaths =
+    [standardLibLocation ++ "/standard", standardLibLocation ++ "/prototype"]
+
+-- ###################################################################### --
+-- Section: Data
+-- ###################################################################### --
+
+-- ###################################################################### --
+-- Section: Functions
+-- ###################################################################### --
+
+{-
+produceProgramFromFile :: [FilePath] -> String -> IO ProgramTable
+produceProgramFromFile filePaths source = do
+    let sourceName = head filePaths
 -}
-preCheckProgramTable :: ProgramTable -> IO ProgramTable
-precheckProgramTable table = do
-let lookupTableTable = fmap buildLookupTable table
-    mapM (precheckAndShowWarnings lookupTableTable) table
-    where
-    precheckAndShowWarnings table p = do
-        (precheckedAST, precheckingWarnings) <-
-            case precheckProgram table p of
-            (Right ast, warnings)  -> return (ast, warnings)
-            (Left error, warnings) -> do
-                showWarnings warnings
-                abort $ show error
-        showWarnings precheckingWarnings
-        return precheckedAST
 
-{- Run type checking on a ProgramTable to produce a type-checked ProgramTable
 
-Param: Program table to run type-checking on
--}
-typeCheckProgramTable :: ProgramTable -> IO ProgramTable
-typecheckProgramTable table = do
-    let lookupTableTable = fmap buildLookupTable table
-    mapM (typecheckAndShowWarnings lookupTableTable) table
+
+produceProgramFromSource :: String -> String -> IO (DBProgramTable) 
+produceProgramFromSource sourceName source = do
+    -- Parse program to produce AST
+    (ast, error) <- case parseEncoreProgram sourceName source of
+        Right ast   -> return (ast, Nothing)
+        Left error  -> return (blankProgram, Just error)
+
+    case error of
+        Just e -> do 
+            print "Failed to parse program"
+            let dbProgram = (makeDBProgram blankProgram [] [])
+            let dbTable = (Map.insert sourceName dbProgram (Map.empty :: DBProgramTable))
+            return (dbTable)
+        Nothing -> do
+            -- Build program table from AST
+            programTable <- buildProgramTable preludePaths preludePaths ast
+            let desugaredTable = fmap desugarProgram programTable
+
+            -- Convert the desugared table into a DBProgramTable
+            let dbTable = convertFromProgramTable desugaredTable
+
+            -- Precheck and typecheck the table
+            precheckedTable <- producerPrecheck dbTable
+            typecheckedTable <- producerTypecheck precheckedTable
+            return (typecheckedTable)
+
+
+{- Returns a blank program  -}
+blankProgram :: Program
+blankProgram = Program{
+    source = "",
+    moduledecl = NoModule,
+    etl = [],
+    imports = [],
+    typedefs = [],
+    functions = [],
+    traits = [],
+    classes = []
+}
+
+
+-- ###################################################################### --
+-- Section: Type checking
+-- ###################################################################### --
+
+producerPrecheckProgram :: (Map.Map FilePath LookupTable) -> DBProgram -> IO (DBProgram)
+producerPrecheckProgram lookupTable program = do
+    case precheckProgram lookupTable (getDBProgram program) of
+        (Right newProgram, warnings)    -> return $ makeDBProgram newProgram [] warnings
+        (Left error, warnings)          -> return $ makeDBProgram  blankProgram [error] warnings 
+
+producerPrecheck :: DBProgramTable -> IO (DBProgramTable)
+producerPrecheck programTable = do
+    let lookupTable = fmap buildLookupTable (convertToProgramTable programTable)
+    mapM (_producerPrecheck lookupTable) programTable
     where
-        typecheckAndShowWarnings table p = do
-        (typecheckedAST, typecheckingWarnings) <-
-            case typecheckProgram table p of
-                (Right (newEnv, ast), warnings) -> return (ast, warnings)
-                (Left error, warnings) -> do
-                showWarnings warnings
-                abort $ show error
-        showWarnings typecheckingWarnings
-        return typecheckedAST
+        _producerPrecheck lookupTable program = do
+            (precheckedProgram) <- (producerPrecheckProgram lookupTable program)
+            return (precheckedProgram)
+
+producerTypecheckProgram :: (Map.Map FilePath LookupTable) -> DBProgram -> IO (DBProgram)
+producerTypecheckProgram lookupTable program = do
+    case typecheckProgram lookupTable (getDBProgram program) of
+        (Right (env, newProgram), warnings) -> return $ makeDBProgram newProgram [] warnings
+        (Left error, warnings)              -> return $ makeDBProgram blankProgram [error] warnings
+
+producerTypecheck :: DBProgramTable -> IO (DBProgramTable)
+producerTypecheck programTable = do
+    let lookupTable = fmap buildLookupTable (convertToProgramTable programTable)
+    mapM (_producerTypecheck lookupTable) programTable
+    where
+        _producerTypecheck lookupTable program = do
+            (typecheckedProgram) <- (producerTypecheckProgram lookupTable program)
+            return (typecheckedProgram)
