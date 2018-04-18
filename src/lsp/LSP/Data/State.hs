59438141059dfc1a4dac318ee4036c697efa2ef3
Christoffer Gustafsson <christoffer.golf@gmail.com>
2018-04-18 13:40:46 +0200

    Continouation of merge between ls-back and service.

--- src/lsp/LSP/Data/State.hs
+++ src/lsp/LSP/Data/State.hs
@@ -1,17 +1,33 @@
 module LSP.Data.State (
+    TextDocumentMap,
     LSPState,
     initial,
-    addTextDocument,
-    changeTextDocument
+    addTextDocument
 ) where
 
+-- ###################################################################### --
+-- Section: Imports
+-- ###################################################################### --
+
+-- Standard
 import Data.Map as Map hiding (foldr)
 
+-- LSP
 import LSP.Data.TextDocument
 
+-- ###################################################################### --
+-- Section: Data
+-- ###################################################################### --
+
+type TextDocumentMap = Map String TextDocument
+
 data LSPState = LSPState {
-    textDocuments :: Map String TextDocument
+    textDocuments :: TextDocumentMap
 } deriving (Show)
+
+-- ###################################################################### --
+-- Section: Functions
+-- ###################################################################### --
 
 initial :: LSPState
 initial = LSPState {
@@ -21,12 +37,3 @@ initial = LSPState {
 addTextDocument :: TextDocument -> LSPState -> LSPState
 addTextDocument newDocument (LSPState textDocuments) =
     LSPState (Map.insert (uri newDocument) newDocument textDocuments)
-
-changeTextDocument :: TextDocumentChange -> LSPState -> LSPState
-changeTextDocument documentChange (LSPState textDocuments) =
-    case Map.lookup (uri documentChange) textDocuments of
-        Nothing -> LSPState textDocuments
-        Just textDocument ->
-            LSPState $ Map.insert (uri documentChange)
-                                  (foldr applyTextDocumentChange textDocument (changes documentChange))
-                                  textDocuments
