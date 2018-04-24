ff2b78c2c44d94cf528a1cc77302f4048fbad960
Alfret <Afisse13@gmail.com>
2018-04-24 18:44:05 +0200

    Updated ProgramInfo to contain range position

--- src/lsp/LSP/Data/Position.hs
+++ src/lsp/LSP/Data/Position.hs
@@ -1,23 +1,33 @@
-module LSP.Data.Position where
+module LSP.Data.Position (
+  Position,
+  Range,
+  inRange,
+  fromSourcePos,
+  fromSourcePosRange
+) where
 
 -- ###################################################################### --
 -- Section: Imports
 -- ###################################################################### --
 
+-- Library
 import Text.Megaparsec(SourcePos,unPos,sourceLine,sourceColumn)
 
 -- ###################################################################### --
 -- Section: Data
 -- ###################################################################### --
 
+{-  -}
 type Position = (Int, Int)
 
+{-  -}
 type Range = (Position, Position)
 
 -- ###################################################################### --
 -- Section: Functions
 -- ###################################################################### --
 
+{-  -}
 inRange :: Position -> Range -> Bool
 inRange pos (start, end)
   | fst pos > fst start && fst pos < fst end = True
@@ -26,5 +36,10 @@ inRange pos (start, end)
   | fst pos == fst start && fst pos == fst end && snd pos >= snd start && snd pos <= snd end = True
   | True = False -- sorry, had to do it
 
+{-  -}
 fromSourcePos :: SourcePos -> Position
 fromSourcePos pos = (fromIntegral (unPos (sourceLine pos)), fromIntegral (unPos (sourceColumn pos)))
+
+{-  -}
+fromSourcePosRange :: SourcePos -> SourcePos -> Range
+fromSourcePosRange from to = (fromSourcePos from, fromSourcePos to)
